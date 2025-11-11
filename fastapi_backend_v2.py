4fae86e1dd50881178fc23e68cc29d9dc462b6c0
21Sameer <146482142+21Sameer@users.noreply.github.com>
2025-11-11 16:34:17 +0500

    Update fastapi_backend_v2.py

--- fastapi_backend_v2.py
+++ fastapi_backend_v2.py
@@ -150,7 +150,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3000",
         "http://127.0.0.1:5501",
-        "http://localhost:5501"
+        "http://localhost:5501",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
